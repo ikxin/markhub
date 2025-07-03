61ac9d3b8a173d46e2e0e7f6cef476ca08fb4ad9
一纸忘忧 <i@ikxin.com>
2025-07-03 16:26:07 +0800

    feat(gravatar): support email convert hash

--- server/routes/gravatar/[hash].ts
+++ server/routes/gravatar/[hash].ts
@@ -1,12 +1,37 @@
-export default defineResponseHandler(async (event) => {
-  const hash = getRouterParam(event, "hash");
-  const { size = 100, s = 100 } = getQuery(event);
+import * as z from "zod/v4";
+import crypto from "crypto";
 
+function getHash(email: string) {
+  return crypto
+    .createHash("md5")
+    .update(email.trim().toLowerCase())
+    .digest("hex");
+}
+
+const paramSchema = z.object({
+  hash: z.union([
+    z.string().regex(/^([a-f0-9]{32}|[a-f0-9]{64})$/i),
+    z.email().transform((email) => getHash(email)),
+  ]),
+});
+
+const querySchema = z.object({
+  size: z.coerce.number().int().min(1).max(2048).optional(),
+  s: z.coerce.number().int().min(1).max(2048).catch(100),
+});
+
+export default defineResponseHandler(async (event) => {
   try {
-    const fetchUrl = `https://secure.gravatar.com/avatar/${hash}.png?s=${
-      size || s || 100
-    }`;
-    const response = await fetch(fetchUrl);
+    const query = await getValidatedQuery(event, querySchema.parse);
+    const params = await getValidatedRouterParams(event, paramSchema.safeParse);
+
+    if (!params.success) throw new Error();
+
+    const response = await fetch(
+      `https://cravatar.com/avatar/${params.data.hash}?s=${
+        query.size || query.s
+      }`
+    );
 
     if (response.ok) {
       return Buffer.from(await response.arrayBuffer());
@@ -14,7 +39,6 @@ export default defineResponseHandler(async (event) => {
       throw new Error();
     }
   } catch {
-    const fallbackKey = "fallback/gravatar.png";
-    return useStorage("assets:server").getItemRaw(fallbackKey);
+    return useStorage("assets:server").getItemRaw("fallback/gravatar.png");
   }
 });
