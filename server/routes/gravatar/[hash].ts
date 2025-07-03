faee774b334c13cfa753a32364fcea473508105a
一纸忘忧 <i@ikxin.com>
2025-07-03 18:00:56 +0800

    feat(gravatar): support all gravatar api params
    https://docs.gravatar.com/sdk/images

--- server/routes/gravatar/[hash].ts
+++ server/routes/gravatar/[hash].ts
@@ -15,23 +15,37 @@ const paramSchema = z.object({
   ]),
 });
 
+/**
+ * Source https://docs.gravatar.com/sdk/images
+ */
 const querySchema = z.object({
-  size: z.coerce.number().int().min(1).max(2048).optional(),
-  s: z.coerce.number().int().min(1).max(2048).catch(100),
+  // Size
+  s: z.string().optional().default("100"),
+  size: z.string().optional(),
+  // Default Image
+  default: z.string().optional(),
+  // Force Default
+  f: z.string().optional(),
+  forcedefault: z.string().optional(),
+  // Rating
+  r: z.string().optional().default("g"),
+  rating: z.string().optional(),
+  // Initials
+  initials: z.string().optional(),
+  name: z.string().optional(),
 });
 
 export default defineResponseHandler(async (event) => {
   try {
-    const query = await getValidatedQuery(event, querySchema.parse);
+    const query = await getValidatedQuery(event, querySchema.safeParse);
     const params = await getValidatedRouterParams(event, paramSchema.safeParse);
 
     if (!params.success) throw new Error();
 
-    const response = await fetch(
-      `https://cravatar.com/avatar/${params.data.hash}?s=${
-        query.size || query.s
-      }`
-    );
+    const queryParams = new URLSearchParams({ ...query.data });
+
+    const fetchUrl = `https://secure.gravatar.com/avatar/${params.data.hash}?${queryParams}`;
+    const response = await fetch(fetchUrl);
 
     if (response.ok) {
       return Buffer.from(await response.arrayBuffer());
