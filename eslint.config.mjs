bf7e0be95deac89145391aef04ddfcfcafb04344
一纸忘忧 <i@ikxin.com>
2025-07-06 22:18:17 +0800

    chore: initialize @nuxt/ui project structure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,21 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt().override('nuxt/javascript', {
-  rules: {
-    'sort-imports': 'error',
-  },
-})
+export default withNuxt()
+  .override('nuxt/javascript', {
+    rules: {
+      'sort-imports': 'error',
+    },
+  })
+  .override('nuxt/typescript/rules', {
+    rules: {
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  })
+  .override('nuxt/vue/rules', {
+    rules: {
+      'vue/html-self-closing': 'off',
+      'vue/multi-word-component-names': 'off',
+    },
+  })
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: '2026-01-01',
+  future: {
+    compatibilityVersion: 4,
+  },
   devtools: { enabled: true },
-  modules: ['nuxt-security', '@nuxt/eslint'],
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/content',
+    '@nuxt/eslint',
+    'nuxt-security',
+    '@nuxtjs/i18n',
+  ],
+  css: ['~/assets/css/main.css'],
+  eslint: {
+    config: {
+      nuxt: {
+        sortConfigKeys: true,
+      },
+    },
+  },
+  fonts: {
+    provider: 'local',
+    experimental: {
+      processCSSVariables: true,
+    },
+  },
   security: {
     corsHandler: {
       origin: '*',
